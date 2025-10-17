f7772d146d405dc1d0355f61afc063290d3b3ca0
EshaanT <eshaantanwar2000@gmail.com>
2025-10-17 11:15:58 +0200

    updating nomenclature and adding masking

--- utils/prunning.py
+++ utils/prunning.py
@@ -1,3 +1,7 @@
+"""Functions for pruning."""
+
+from __future__ import annotations
+
 import numpy as np
 import torch
 import torch.nn as nn
@@ -8,14 +12,21 @@ def make_mask(model:nn.Module)->dict:
     current_mask={name:torch.ones_like(para) for name,para in model.state_dict().items() if 'weight' in name}
     return current_mask
 
-def global_pruning_by_percentage(model:nn.Module,percentage:int=20,current_mask:bool=False)->dict:
-    """Applies pruning by percentage."""
-    current_weights=[]
+def apply_mask(model:nn.Module,mask:dict)->None:
+    """Masks weights based on the mask dictionary."""
+    with torch.no_grad():
+        for name,para in model.named_parameters():
+            if name in mask:
+                para.mul_(mask[name])
 
+def global_pruning_by_percentage(model:nn.Module,percentage:int=20,current_mask:bool=False)->dict:
+    """Applies pruning for VGG or ResNet.Specifically, If prunes the p lowest magnitude weights in the entire network."""
+    current_weights=[]
     for name,param in model.named_parameters():
         if 'weight' in name:
             masked=param if current_mask is None else param*current_mask.get(name,1)
             current_weights += list(masked.abs().flatten().cpu().detach().numpy())
+
     threshold = np.percentile(current_weights, percentage)
     new_mask = {}
     for name, param in model.named_parameters():
